7be8c8454f96098f50d5c4bc491ff9e5b7b1ac93
nicole pizzano <pizzano.nicole@gmail.com>
2022-04-18 12:39:47 -0400

    saving current work to github

---
+++ ethereum/contracts/EmotionalShapes.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract EmotionalShapes is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("EmotionalShapes", "ESS") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return   "https://ef8c-73-175-77-68.ngrok.io/api/erc721/1";
+    }
+
+
+    function mint(address to)
+        public returns (uint256)
+    {
+        require(_tokenIdCounter.current() < 3); 
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenIdCounter.current());
+
+        return _tokenIdCounter.current();
+    }
+}
